3340c99b5eb7353be673bb0ac7788c098530254f
FelipeSoft <felipedecastrolima2@gmail.com>
2023-07-26 17:51:07 -0300

    Correção de um evento

--- src/components/features/Task.tsx
+++ src/components/features/Task.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const Task = ({ id, title, done, onClick, toggleItem }: Props) => {
     return(
         <div className="flex items-center mb-4">
-            <input type="checkbox" className="w-6 h-6 mr-4" checked={done} onClick={toggleItem}/><p className="pl-2 w-full flex justify-between items-center bg-white h-8 text-black">{title} <button className="bg-red-500 h-full px-5 text-white" onClick={onClick}>Deletar</button></p>
+            <input type="checkbox" className="w-6 h-6 mr-4" checked={done} onChange={toggleItem}/><p className="pl-2 w-full flex justify-between items-center bg-white h-8 text-black">{title} <button className="bg-red-500 h-full px-5 text-white" onClick={onClick}>Deletar</button></p>
         </div>
     );
 }
